76c2e85cd127c50be5afeacbe38669a5e6549fa1
Hartley Jean-Aimee <hect1c17@gmail.com>
2012-12-30 11:53:42 -0500

    oscommerce v2.3.3

---
+++ catalog/admin/includes/configure.php
@@ -0,0 +1,35 @@
+<?php
+  define('HTTP_SERVER', 'http://nan.oscommerce.com');
+  define('HTTP_CATALOG_SERVER', 'http://nan.oscommerce.com');
+  define('HTTPS_CATALOG_SERVER', 'http://nan.oscommerce.com');
+  define('ENABLE_SSL_CATALOG', 'false');
+  define('DIR_FS_DOCUMENT_ROOT', '/Users/Hect1c/Sites/pearly-girl.com/catalog/');
+  define('DIR_WS_ADMIN', '/admin/');
+  define('DIR_FS_ADMIN', '/Users/Hect1c/Sites/pearly-girl.com/catalog/admin/');
+  define('DIR_WS_CATALOG', '/');
+  define('DIR_FS_CATALOG', '/Users/Hect1c/Sites/pearly-girl.com/catalog/');
+  define('DIR_WS_IMAGES', 'images/');
+  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+  define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
+  define('DIR_WS_INCLUDES', 'includes/');
+  define('DIR_WS_BOXES', DIR_WS_INCLUDES . 'boxes/');
+  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+  define('DIR_WS_CATALOG_LANGUAGES', DIR_WS_CATALOG . 'includes/languages/');
+  define('DIR_FS_CATALOG_LANGUAGES', DIR_FS_CATALOG . 'includes/languages/');
+  define('DIR_FS_CATALOG_IMAGES', DIR_FS_CATALOG . 'images/');
+  define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
+  define('DIR_FS_BACKUP', DIR_FS_ADMIN . 'backups/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'root');
+  define('DB_SERVER_PASSWORD', 'root');
+  define('DB_DATABASE', 'pearlygirl');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('CFG_TIME_ZONE', 'America/New_York');
+?>
