6c6a49126cde7cfc7b812cf7017744499e088d1a
Hartley Jean-Aimee <hect1c17@gmail.com>
2013-01-03 22:11:50 -0500

    opencart 1.5.4.1 installation

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://nans.com/admin/');
+define('HTTP_CATALOG', 'http://nans.com/');
+define('HTTP_IMAGE', 'http://nans.com/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://nans.com/admin/');
+define('HTTPS_CATALOG', 'http://nans.com/');
+define('HTTPS_IMAGE', 'http://nans.com/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Hect1c/Sites/nans/admin/');
+define('DIR_SYSTEM', '/Users/Hect1c/Sites/nans/system/');
+define('DIR_DATABASE', '/Users/Hect1c/Sites/nans/system/database/');
+define('DIR_LANGUAGE', '/Users/Hect1c/Sites/nans/admin/language/');
+define('DIR_TEMPLATE', '/Users/Hect1c/Sites/nans/admin/view/template/');
+define('DIR_CONFIG', '/Users/Hect1c/Sites/nans/system/config/');
+define('DIR_IMAGE', '/Users/Hect1c/Sites/nans/image/');
+define('DIR_CACHE', '/Users/Hect1c/Sites/nans/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Hect1c/Sites/nans/download/');
+define('DIR_LOGS', '/Users/Hect1c/Sites/nans/system/logs/');
+define('DIR_CATALOG', '/Users/Hect1c/Sites/nans/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://nans.com/');
+define('HTTP_IMAGE', 'http://nans.com/image/');
+define('HTTP_ADMIN', 'http://nans.com/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://nans.com/');
+define('HTTPS_IMAGE', 'http://nans.com/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Hect1c/Sites/nans/catalog/');
+define('DIR_SYSTEM', '/Users/Hect1c/Sites/nans/system/');
+define('DIR_DATABASE', '/Users/Hect1c/Sites/nans/system/database/');
+define('DIR_LANGUAGE', '/Users/Hect1c/Sites/nans/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Hect1c/Sites/nans/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Hect1c/Sites/nans/system/config/');
+define('DIR_IMAGE', '/Users/Hect1c/Sites/nans/image/');
+define('DIR_CACHE', '/Users/Hect1c/Sites/nans/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Hect1c/Sites/nans/download/');
+define('DIR_LOGS', '/Users/Hect1c/Sites/nans/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
