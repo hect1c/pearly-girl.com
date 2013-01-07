143579249d48c38c26fc182d7cfccc941ab3c5d4
Hartley Jean-Aimee <hect1c17@gmail.com>
2013-01-07 00:32:01 -0500

    Removed uncessary files.

--- admin/config.php
+++ admin/config.php
@@ -1,26 +1,26 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://nans.com/admin/');
-define('HTTP_CATALOG', 'http://nans.com/');
-define('HTTP_IMAGE', 'http://nans.com/image/');
+define('HTTP_SERVER', 'http://nan.opencart.com/admin/');
+define('HTTP_CATALOG', 'http://nan.opencart.com/');
+define('HTTP_IMAGE', 'http://nan.opencart.com/image/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://nans.com/admin/');
-define('HTTPS_CATALOG', 'http://nans.com/');
-define('HTTPS_IMAGE', 'http://nans.com/image/');
+define('HTTPS_SERVER', 'http://nan.opencart.com/admin/');
+define('HTTPS_CATALOG', 'http://nan.opencart.com/');
+define('HTTPS_IMAGE', 'http://nan.opencart.com/image/');
 
 // DIR
-define('DIR_APPLICATION', '/Users/Hect1c/Sites/nans/admin/');
-define('DIR_SYSTEM', '/Users/Hect1c/Sites/nans/system/');
-define('DIR_DATABASE', '/Users/Hect1c/Sites/nans/system/database/');
-define('DIR_LANGUAGE', '/Users/Hect1c/Sites/nans/admin/language/');
-define('DIR_TEMPLATE', '/Users/Hect1c/Sites/nans/admin/view/template/');
-define('DIR_CONFIG', '/Users/Hect1c/Sites/nans/system/config/');
-define('DIR_IMAGE', '/Users/Hect1c/Sites/nans/image/');
-define('DIR_CACHE', '/Users/Hect1c/Sites/nans/system/cache/');
-define('DIR_DOWNLOAD', '/Users/Hect1c/Sites/nans/download/');
-define('DIR_LOGS', '/Users/Hect1c/Sites/nans/system/logs/');
-define('DIR_CATALOG', '/Users/Hect1c/Sites/nans/catalog/');
+define('DIR_APPLICATION', '/Users/Hect1c/Sites/pearly-girl.com/admin/');
+define('DIR_SYSTEM', '/Users/Hect1c/Sites/pearly-girl.com/system/');
+define('DIR_DATABASE', '/Users/Hect1c/Sites/pearly-girl.com/system/database/');
+define('DIR_LANGUAGE', '/Users/Hect1c/Sites/pearly-girl.com/admin/language/');
+define('DIR_TEMPLATE', '/Users/Hect1c/Sites/pearly-girl.com/admin/view/template/');
+define('DIR_CONFIG', '/Users/Hect1c/Sites/pearly-girl.com/system/config/');
+define('DIR_IMAGE', '/Users/Hect1c/Sites/pearly-girl.com/image/');
+define('DIR_CACHE', '/Users/Hect1c/Sites/pearly-girl.com/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Hect1c/Sites/pearly-girl.com/download/');
+define('DIR_LOGS', '/Users/Hect1c/Sites/pearly-girl.com/system/logs/');
+define('DIR_CATALOG', '/Users/Hect1c/Sites/pearly-girl.com/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
--- config.php
+++ config.php
@@ -1,24 +1,24 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://nans.com/');
-define('HTTP_IMAGE', 'http://nans.com/image/');
-define('HTTP_ADMIN', 'http://nans.com/admin/');
+define('HTTP_SERVER', 'http://nan.opencart.com/');
+define('HTTP_IMAGE', 'http://nan.opencart.com/image/');
+define('HTTP_ADMIN', 'http://nan.opencart.com/admin/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://nans.com/');
-define('HTTPS_IMAGE', 'http://nans.com/image/');
+define('HTTPS_SERVER', 'http://nan.opencart.com/');
+define('HTTPS_IMAGE', 'http://nan.opencart.com/image/');
 
 // DIR
-define('DIR_APPLICATION', '/Users/Hect1c/Sites/nans/catalog/');
-define('DIR_SYSTEM', '/Users/Hect1c/Sites/nans/system/');
-define('DIR_DATABASE', '/Users/Hect1c/Sites/nans/system/database/');
-define('DIR_LANGUAGE', '/Users/Hect1c/Sites/nans/catalog/language/');
-define('DIR_TEMPLATE', '/Users/Hect1c/Sites/nans/catalog/view/theme/');
-define('DIR_CONFIG', '/Users/Hect1c/Sites/nans/system/config/');
-define('DIR_IMAGE', '/Users/Hect1c/Sites/nans/image/');
-define('DIR_CACHE', '/Users/Hect1c/Sites/nans/system/cache/');
-define('DIR_DOWNLOAD', '/Users/Hect1c/Sites/nans/download/');
-define('DIR_LOGS', '/Users/Hect1c/Sites/nans/system/logs/');
+define('DIR_APPLICATION', '/Users/Hect1c/Sites/pearly-girl.com/catalog/');
+define('DIR_SYSTEM', '/Users/Hect1c/Sites/pearly-girl.com/system/');
+define('DIR_DATABASE', '/Users/Hect1c/Sites/pearly-girl.com/system/database/');
+define('DIR_LANGUAGE', '/Users/Hect1c/Sites/pearly-girl.com/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Hect1c/Sites/pearly-girl.com/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Hect1c/Sites/pearly-girl.com/system/config/');
+define('DIR_IMAGE', '/Users/Hect1c/Sites/pearly-girl.com/image/');
+define('DIR_CACHE', '/Users/Hect1c/Sites/pearly-girl.com/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Hect1c/Sites/pearly-girl.com/download/');
+define('DIR_LOGS', '/Users/Hect1c/Sites/pearly-girl.com/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysql');
